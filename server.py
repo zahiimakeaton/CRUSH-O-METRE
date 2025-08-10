a8db3533de5368514ea880b003cbddc06f146563
zahiimakeaton <zahiaman@gmail.com>
2025-08-10 15:11:23 +0530

    Add Flask API server

--- server.py
+++ server.py
@@ -106,4 +106,4 @@ Now analyze this chat message: "{user_text}"
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
